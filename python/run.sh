256599a86c5dd20f0817822b5176724322078e10
Christopher Isene <christopher.isene@gmail.com>
2024-06-11 17:03:30 +0200

    New bash-script for automating commits

---
+++ python/run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+# Sort entries
+python3 ./entries-sort.py
+
+
+# Render JSON
+python3 ./entries-to-json.py
+
+# Render CSV
+python3 ./entries-to-csv.py
+
+# Render TSV
+python3 ./entries-to-tsv.py
+
+# Render XML
+python3 ./entries-to-xml.py
+
+
+# Add to git
+git add ./yaml/entries.yaml
+git add ./json/swish-123-datasource.json
+git add ./text/swish-123-datasource.csv
+git add ./test/swish-123-datasource.tsv
+git add ./xml/swish-123-datasource.xml
+
+git commit -m "Automated commit"
+
+git push
