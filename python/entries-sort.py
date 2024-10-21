3877b5721a877dc12ae40e404007e2d2126258b1
Christopher Isene <christopher.isene@gmail.com>
2024-10-21 14:07:53 +0200

    Update

--- python/entries-sort.py
+++ python/entries-sort.py
@@ -70,6 +70,8 @@ def ReplaceFilter(data):
 
   data = re.sub(r"\sförsamling$", " Församling", data, flags=re.IGNORECASE)
   data = re.sub(r"\spastorat$", " Pastorat", data, flags=re.IGNORECASE)
+  data = re.sub(r"\sdomkyrkopastorat$", " Domkyrkopastorat", data, flags=re.IGNORECASE)
+
 
   data = re.sub(r"\sscoutkår$", " Scoutkår", data, flags=re.IGNORECASE)
   data = re.sub(r"\scykelklubb$", " Cykelklubb", data, flags=re.IGNORECASE)
