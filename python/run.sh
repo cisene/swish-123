4637fa370fea2c5576369b5012f96e46fe812b13
Christopher Isene <christopher.isene@gmail.com>
2026-01-03 18:58:46 +0100

    cleanup

--- python/run.sh
+++ python/run.sh
@@ -26,14 +26,6 @@ python3 ./entries-to-tsv.py
 # Render XML
 python3 ./entries-to-xml.py
 
-# Render Markdown
-#python3 ./entries-to-markdown.py
-
-
-# Render PDF from MD
-#pandoc --pdf-engine=pdfroff --toc-depth=1 ../swish-123.md -o ../swish-123.pdf
-
-#iconv -t utf-8 ../swish-123.md | pandoc --pdf-engine=pdfroff --toc-depth=1 -o ../swish-123.pdf | iconv -f utf-8
 
 # Add to git
 git add ../yaml/entries.yaml
@@ -44,7 +36,7 @@ git add ../text/swish-123-datasource.tsv
 git add ../xml/swish-123-datasource.xml
 git add ../yaml/masterdata-organisations.yaml
 
-git add ../swish-123.md
+#git add ../swish-123.md
 #git add ../swish-123.pdf
 
 git commit -m "Automated commit"
